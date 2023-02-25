d56f6651687aef1b8a8d9966366e128794efee62
ta <kishiyama.t@gmail.com>
2023-02-25 19:11:39 +0900

    timeline var から audio は取れる気がする

---
+++ book/timeline_variables.js
@@ -0,0 +1,130 @@
+var timeline_variables = [
+ {
+   "a": "espo-1.wav",
+   "b": "esupo-3.wav",
+   "x": "espo-2.wav",
+   "type": "tgt",
+   "id": 1,
+   "cor": "a"
+ },
+ {
+   "a": "esupo-1.wav",
+   "b": "espo-3.wav",
+   "x": "espo-2.wav",
+   "type": "tgt",
+   "id": 2,
+   "cor": "b"
+ },
+ {
+   "a": "esupo-1.wav",
+   "b": "espo-3.wav",
+   "x": "esupo-2.wav",
+   "type": "tgt",
+   "id": 3,
+   "cor": "a"
+ },
+ {
+   "a": "espo-1.wav",
+   "b": "esupo-3.wav",
+   "x": "esupo-2.wav",
+   "type": "tgt",
+   "id": 4,
+   "cor": "b"
+ },
+ {
+   "a": "epso-1.wav",
+   "b": "epuso-3.wav",
+   "x": "epso-2.wav",
+   "type": "tgt",
+   "id": 5,
+   "cor": "a"
+ },
+ {
+   "a": "epuso-1.wav",
+   "b": "epso-3.wav",
+   "x": "epso-2.wav",
+   "type": "tgt",
+   "id": 6,
+   "cor": "b"
+ },
+ {
+   "a": "epuso-1.wav",
+   "b": "epso-3.wav",
+   "x": "epuso-2.wav",
+   "type": "tgt",
+   "id": 7,
+   "cor": "a"
+ },
+ {
+   "a": "epso-1.wav",
+   "b": "epuso-3.wav",
+   "x": "epuso-2.wav",
+   "type": "tgt",
+   "id": 8,
+   "cor": "b"
+ },
+ {
+   "a": "ezbo-1.wav",
+   "b": "ezubo-3.wav",
+   "x": "ezbo-2.wav",
+   "type": "flr",
+   "id": 9,
+   "cor": "a"
+ },
+ {
+   "a": "ezubo-1.wav",
+   "b": "ezbo-3.wav",
+   "x": "ezbo-2.wav",
+   "type": "flr",
+   "id": 10,
+   "cor": "b"
+ },
+ {
+   "a": "ezubo-1.wav",
+   "b": "ezbo-3.wav",
+   "x": "ezubo-2.wav",
+   "type": "flr",
+   "id": 11,
+   "cor": "a"
+ },
+ {
+   "a": "ezbo-1.wav",
+   "b": "ezubo-3.wav",
+   "x": "ezubo-2.wav",
+   "type": "flr",
+   "id": 12,
+   "cor": "b"
+ },
+ {
+   "a": "ebzo-1.wav",
+   "b": "ebuzo-3.wav",
+   "x": "ebzo-2.wav",
+   "type": "flr",
+   "id": 13,
+   "cor": "a"
+ },
+ {
+   "a": "ebuzo-1.wav",
+   "b": "ebzo-3.wav",
+   "x": "ebzo-2.wav",
+   "type": "flr",
+   "id": 14,
+   "cor": "b"
+ },
+ {
+   "a": "ebuzo-1.wav",
+   "b": "ebzo-3.wav",
+   "x": "ebuzo-2.wav",
+   "type": "flr",
+   "id": 15,
+   "cor": "a"
+ },
+ {
+   "a": "ebzo-1.wav",
+   "b": "ebuzo-3.wav",
+   "x": "ebuzo-2.wav",
+   "type": "flr",
+   "id": 16,
+   "cor": "b"
+ }
+]
