b5e3bc6a74c19389671aa42a75c703a6ee1b9cc9
ta <kishiyama.t@gmail.com>
2023-02-25 18:52:07 +0900

    updated

---
+++ book/experiment.js
@@ -0,0 +1,92 @@
+// list_audio_preload は前でまとめる
+// list_audio_preload は前でまとめる
+var list_audio_preload = ['espo-1.wav', 'esupo-2.wav', 'esupo-3.wav']
+
+var jsPsych = initJsPsych({
+  use_webaudio: false,
+  on_finish: function(){
+    jsPsych.data.displayData();
+  }
+});
+
+var welcome = {
+    type: jsPsychHtmlKeyboardResponse,
+    choices: [' '],
+    stimulus: `
+    <p>　実験へのご参加ありがとうございます。
+    この弁別実験の各課題では a -> b -> x という順序で 3つの音を聞いてもらい、
+    3つ目の音(x)が似ているのが
+    (a)1つ目の音か (b)2つ目の音か を a か b のキーで答えてもらいます。
+    静かな環境で、可能な場合はイヤホンなどの装着をお願いいたします。
+    スペースキーで始めます。
+    </p>
+  `,
+};
+
+var fixation = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus: '<div style="font-size:60px;">+</div>',
+    choices: jsPsych.NO_KEYS,
+    trial_duration: 1000,
+}
+
+var trial_a = {
+    type: jsPsychAudioKeyboardResponse,
+    stimulus: jsPsych.timelineVariable('a'),
+    choices: jsPsych.NO_KEYS,
+    trial_ends_after_audio: true,
+    post_trial_gap: 200,
+};
+
+var trial_b = {
+    type: jsPsychAudioKeyboardResponse,
+    stimulus: jsPsych.timelineVariable('b'),
+    choices: jsPsych.NO_KEYS,
+    trial_ends_after_audio: true,
+    post_trial_gap: 200,
+};
+
+var trial_x = {
+    type: jsPsychAudioKeyboardResponse,
+    stimulus: jsPsych.timelineVariable('b'),
+    choices: jsPsych.NO_KEYS,
+    trial_ends_after_audio: true,
+    // post_trial_gap: 200,  // 最後は不要
+};
+
+var axb_question = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus: '音声呈示は a -> b -> x の順でした。',
+    choices: ['a', 'b'],
+    prompt: "<p> 3つ目の音(x)は1つ目の音(a)と2つ目の音(b)のどちらに似ていますか。</p>",
+    data: {
+        task: 'axb', // production--perception-categorization
+        type: jsPsych.timelineVariable('type'), // filler--target
+        item_id: jsPsych.timelineVariable('item_id'),
+        correct: jsPsych.timelineVariable('correct'),
+    },
+};
+
+var goodbye = {
+    type: 'jsPsychHtmlKeyboardResponse',
+    choices: [' '],
+    stimulus: `
+    <p>　実験へのご参加ありがとうございます。
+    スペースキーで終わります。
+    </p>
+  `,
+};
+
+var axb_trial = {
+  timeline: [fixation, trial_a, trial_b, trial_x, axb_question],
+  timeline_variables: timeline_variables
+};
+
+var preload = {
+    type: jsPsychPreload,
+    audio: list_audio_preload,
+}
+
+var timeline = [ welcome, axb_trial, goodbye ]
+jsPsych.run(timeline)
+
