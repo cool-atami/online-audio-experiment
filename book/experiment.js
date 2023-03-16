f585786de7f88f1c2c89e18ba389244911f5f77a
kishiyamat <kishiyama.t@hotmail.com>
2023-03-17 02:15:35 +0900

    サンプルのABX実験追加 (#19)
    * updated
    
    * Update experiment.js
    
    * timeline var から audio は取れる気がする
    
    * 読み込む対象のリストは timeline_variables.js から読み込む
    
    * 順序変更
    
    * updated json
    
    * added sound files and updated script
    
    * ベタがきバージョン
    
    * updated
    
    * updated
    
    * done

---
+++ book/experiment.js
@@ -0,0 +1,94 @@
+var jsPsych = initJsPsych({
+    use_webaudio: false,
+    on_finish: function() {
+        jsPsych.data.displayData();
+    }
+});
+
+// TODO: timeline_variables は事前に読み込んでいる前提
+var list_audio_preload = timeline_variables.map(function(obj) {
+    return [obj.a, obj.b, obj.x];
+}).flat(1);
+
+var welcome = {
+    type: jsPsychHtmlKeyboardResponse,
+    choices: [' '],
+    stimulus: `
+    <p>　実験へのご参加ありがとうございます。
+    この弁別実験の各課題では a -> b -> x という順序で 3つの音を聞いてもらい、
+    3つ目の音(x)が似ているのが
+    (a)1つ目の音か (b)2つ目の音か を a か b のキーで答えてもらいます。
+    静かな環境で、可能な場合はイヤホンなどの装着をお願いいたします。
+    スペースキーで始めます。
+    </p>
+  `,
+};
+
+var fixation = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus: '<div style="font-size:60px;">+</div>',
+    choices: "NO_KEYS",
+    trial_duration: 1000,
+}
+
+var trial_a = {
+    type: jsPsychAudioKeyboardResponse,
+    stimulus: jsPsych.timelineVariable('a'),
+    choices: "NO_KEYS",
+    trial_ends_after_audio: true,
+    post_trial_gap: 200,
+};
+
+var trial_b = {
+    type: jsPsychAudioKeyboardResponse,
+    stimulus: jsPsych.timelineVariable('b'),
+    choices: "NO_KEYS",
+    trial_ends_after_audio: true,
+    post_trial_gap: 200,
+};
+
+var trial_x = {
+    type: jsPsychAudioKeyboardResponse,
+    stimulus: jsPsych.timelineVariable('x'),
+    choices: "NO_KEYS",
+    trial_ends_after_audio: true,
+    // post_trial_gap: 200,  // 最後は不要
+};
+
+var axb_question = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus: '音声呈示は a -> b -> x の順でした。',
+    choices: ['a', 'b'],
+    prompt: "<p> 3つ目の音(x)は1つ目の音(a)と2つ目の音(b)のどちらに似ていますか。</p>",
+    data: {
+        task: 'axb', // production--perception-categorization
+        type: jsPsych.timelineVariable('type'), // filler--target
+        item_id: jsPsych.timelineVariable('sid'),
+        correct: jsPsych.timelineVariable('cor'),
+    },
+};
+
+var goodbye = {
+    type: jsPsychHtmlKeyboardResponse,
+    choices: [' '],
+    stimulus: `
+    <p>　実験へのご参加ありがとうございます。
+    スペースキーで終わります。
+    </p>
+  `,
+};
+
+var shuffled_list= jsPsych.randomization.shuffle(timeline_variables);
+var abx_trial = {
+    timeline: [fixation, trial_a, trial_b, trial_x, axb_question],
+    timeline_variables: shuffled_list
+};
+
+var preload = {
+    type: jsPsychPreload,
+    audio: list_audio_preload,
+}
+
+var timeline = [preload, welcome, abx_trial, goodbye];
+jsPsych.run(timeline);
+
---
+++ book/timeline_variables.js
@@ -0,0 +1,131 @@
+var timeline_variables = [
+ {
+   "a": "espo-1.wav",
+   "b": "esupo-3.wav",
+   "x": "espo-2.wav",
+   "type": "tgt",
+   "sid": 1,
+   "cor": "a"
+ },
+ {
+   "a": "esupo-1.wav",
+   "b": "espo-3.wav",
+   "x": "espo-2.wav",
+   "type": "tgt",
+   "sid": 2,
+   "cor": "b"
+ },
+ {
+   "a": "esupo-1.wav",
+   "b": "espo-3.wav",
+   "x": "esupo-2.wav",
+   "type": "tgt",
+   "sid": 3,
+   "cor": "a"
+ },
+ {
+   "a": "espo-1.wav",
+   "b": "esupo-3.wav",
+   "x": "esupo-2.wav",
+   "type": "tgt",
+   "sid": 4,
+   "cor": "b"
+ },
+ {
+   "a": "epso-1.wav",
+   "b": "epuso-3.wav",
+   "x": "epso-2.wav",
+   "type": "tgt",
+   "sid": 5,
+   "cor": "a"
+ },
+ {
+   "a": "epuso-1.wav",
+   "b": "epso-3.wav",
+   "x": "epso-2.wav",
+   "type": "tgt",
+   "sid": 6,
+   "cor": "b"
+ },
+ {
+   "a": "epuso-1.wav",
+   "b": "epso-3.wav",
+   "x": "epuso-2.wav",
+   "type": "tgt",
+   "sid": 7,
+   "cor": "a"
+ },
+ {
+   "a": "epso-1.wav",
+   "b": "epuso-3.wav",
+   "x": "epuso-2.wav",
+   "type": "tgt",
+   "sid": 8,
+   "cor": "b"
+ },
+ {
+   "a": "ezbo-1.wav",
+   "b": "ezubo-3.wav",
+   "x": "ezbo-2.wav",
+   "type": "flr",
+   "sid": 9,
+   "cor": "a"
+ },
+ {
+   "a": "ezubo-1.wav",
+   "b": "ezbo-3.wav",
+   "x": "ezbo-2.wav",
+   "type": "flr",
+   "sid": 10,
+   "cor": "b"
+ },
+ {
+   "a": "ezubo-1.wav",
+   "b": "ezbo-3.wav",
+   "x": "ezubo-2.wav",
+   "type": "flr",
+   "sid": 11,
+   "cor": "a"
+ },
+ {
+   "a": "ezbo-1.wav",
+   "b": "ezubo-3.wav",
+   "x": "ezubo-2.wav",
+   "type": "flr",
+   "sid": 12,
+   "cor": "b"
+ },
+ {
+   "a": "ebzo-1.wav",
+   "b": "ebuzo-3.wav",
+   "x": "ebzo-2.wav",
+   "type": "flr",
+   "sid": 13,
+   "cor": "a"
+ },
+ {
+   "a": "ebuzo-1.wav",
+   "b": "ebzo-3.wav",
+   "x": "ebzo-2.wav",
+   "type": "flr",
+   "sid": 14,
+   "cor": "b"
+ },
+ {
+   "a": "ebuzo-1.wav",
+   "b": "ebzo-3.wav",
+   "x": "ebuzo-2.wav",
+   "type": "flr",
+   "sid": 15,
+   "cor": "a"
+ },
+ {
+   "a": "ebzo-1.wav",
+   "b": "ebuzo-3.wav",
+   "x": "ebuzo-2.wav",
+   "type": "flr",
+   "sid": 16,
+   "cor": "b"
+ }
+];
+
